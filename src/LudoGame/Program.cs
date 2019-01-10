304299ab18f4f14281682bd7dd79d3fb4c85ad64
Phatchari <gataij@hotmail.com>
2019-01-10 13:52:19 +0100

    lag till kod ifrån den andra projektet

--- src/LudoGame/Program.cs
+++ src/LudoGame/Program.cs
@@ -7,6 +7,31 @@ namespace LudoGame
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            int nrOfPlayer;
+
+            Console.WriteLine("How many player?");
+            int countOfPlayer = int.Parse(Console.ReadLine());
+
+            string player;
+
+            if (nrOfPlayer <= 4)
+            {
+                Console.WriteLine("Game start");
+            }
+            else
+            {
+                Console.WriteLine("To many player...");
+                // vill att man ska kunna fortsätta att skriva in hur många spelare man är om man blir för många....
+                //Console.WriteLine("How many player?");
+                //nrOfPlayer = int.Parse(Console.ReadLine());
+            }
+
+            Random randomNr = new Random();
+            int rand_dice = randomNr.Next(1, 7);
+
+
+            Console.ReadKey();
         }
     }
 }
